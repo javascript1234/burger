ef6ea20746e2d96ab39f830a61783d0bbee1e54d
amaraa <learn@1234.mn>
2020-03-27 23:08:10 +0800

    Updates

---
+++ precache-manifest.919c75b7eef9764877d899201124a547.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "b66dc2fad6919780e53abad1b181076d",
+    "url": "/burger/index.html"
+  },
+  {
+    "revision": "ab8ed3b46156520623b3",
+    "url": "/burger/static/css/main.1263f036.chunk.css"
+  },
+  {
+    "revision": "d8344b7e613fa04156e0",
+    "url": "/burger/static/js/2.a7103e70.chunk.js"
+  },
+  {
+    "revision": "928d7b5eb39d16fe9a880722c974b51e",
+    "url": "/burger/static/js/2.a7103e70.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "ab8ed3b46156520623b3",
+    "url": "/burger/static/js/main.146b52be.chunk.js"
+  },
+  {
+    "revision": "d4390a2908c1cac1ad60",
+    "url": "/burger/static/js/runtime-main.b5950f3a.js"
+  },
+  {
+    "revision": "b8503d262bffbfb7c67fd6762963e7d1",
+    "url": "/burger/static/media/burger-logo.b8503d26.png"
+  }
+]);
